c61e5a4f3beeecee4295d3042b693e211c4bee35
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-02-17 09:27:08 -0500

    Base App Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import './App.css';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Loading...</div>
+        )
+    }
+}
+
+
+export default App;
