a031add73aa4b9241b28e879a7c393be646167bf
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-02-20 13:45:20 -0500

    App structure setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { ButtonGroup, Button } from 'reactstrap';
 import './App.css';
 
+const pageComponentMap = new Map();
 
-class App extends React.Component {
+class NumPlayerSelect extends React.Component {
     render() {
         return (
-            <div>Loading...</div>
+            <div className="numSelectButtons">
+                <h3> Select Number of Players: </h3>
+                <ButtonGroup>
+                    <Button
+                        color="primary"
+                    >
+                        3
+                    </Button>
+                    <Button
+                        color="primary"
+                    >
+                        4
+                    </Button>
+                    <Button
+                        color="primary"
+                    >
+                        5
+                    </Button>
+                    <Button
+                        color="primary"
+                    >
+                        6
+                    </Button>
+                    </ButtonGroup>
+                </div>
         )
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // loads PageComponent Map
+        pageComponentMap.set("numPlayerSelect", <NumPlayerSelect />);
+
+        // state init
+        this.state = { page: "numPlayerSelect", sessionKey: null };
+    }
+
+    generateSesionKey() {
+        // get a session key
+        this.setState({ sessionKey: "" });
+    }
+
+    render() {
+        return pageComponentMap.get(this.state.page);
     }
 }
 
