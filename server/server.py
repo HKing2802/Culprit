7792878a9c85ee12c3a7fce6607e8c9778b753be
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-02-21 12:57:26 -0500

    Added data load to startup and starting resource

--- server/server.py
+++ server/server.py
@@ -2,16 +2,18 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from api.db_utils import *
-from api.Culprit_api import build_tables
+from api.Culprit_api import *
 
 app = Flask(__name__) #create Flask instance
 
 api = Api(app) #api router
 
-# api.add_resource(ExampleApi,'/example_api')
+api.add_resource(example,'/example_api')
 
 if __name__ == '__main__':
-    print("Loading Database")
+    print("Building Database")
     build_tables()
+    print("Loading Data")
+    load_game_data()
     print("Starting Flask")
     app.run(debug=True), #starts Flask
