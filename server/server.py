e7c23a70d8c17a5980194221baf7f1fb741e0c46
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-02-21 00:10:16 -0500

    Added database setup to server start

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from api.db_utils import *
-
+from api.Culprit_api import build_tables
 
 app = Flask(__name__) #create Flask instance
 
@@ -11,5 +11,7 @@ api = Api(app) #api router
 # api.add_resource(ExampleApi,'/example_api')
 
 if __name__ == '__main__':
-    print("Starting Flask");
+    print("Loading Database")
+    build_tables()
+    print("Starting Flask")
     app.run(debug=True), #starts Flask
