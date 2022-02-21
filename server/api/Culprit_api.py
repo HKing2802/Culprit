3ea3e96b3dff6f1938b922580d7fb811cc33b6d2
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-02-21 12:57:14 -0500

    Added data load and starting resource

--- server/api/Culprit_api.py
+++ server/api/Culprit_api.py
@@ -1,8 +1,15 @@
 from flask_restful import Resource, reqparse
-from api.db_utils import exec_sql_file
+from api.db_utils import exec_sql_file, exec_get_all
 
 # Data Loading
 def build_tables():
     exec_sql_file("src/schema.sql")
 
+def load_game_data():
+    exec_sql_file("src/data.sql")
+
 # Resource Classes
+
+class example(Resource):
+    def get(self):
+        return exec_get_all("SELECT * FROM weapons;")
