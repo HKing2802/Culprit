dec5b9397b159391ce249aa9f9d130adcd3b896d
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-02-21 12:57:40 -0500

    Added weapon, location and victim data

---
+++ server/src/data.sql
@@ -0,0 +1,33 @@
+
+INSERT INTO weapons (id, name, tag1, tag2) VALUES
+	(1, 'Pistol', 'One-Handed', 'Ranged'),
+	(2, 'Sword', 'One-Handed', 'Sharp'),
+	(3, 'Hammer', 'One-Handed', 'Blunt'),
+	(4, 'Rifle', 'Two-Handed', 'Ranged'),
+	(5, 'Battle Axe', 'Two-Handed', 'Sharp'),
+	(6, 'Bat', 'Two-Handed', 'Blunt'),
+	(7, 'Dart', 'Quick', 'Ranged'),
+	(8, 'Dagger', 'Quick', 'Sharp'),
+	(9, 'Rock', 'Quick', 'Blunt');
+
+INSERT INTO locations (id, name, tag1, tag2) VALUES
+	(1, 'Shed', 'Exterior', 'Core'),
+	(2, 'Pool', 'Exterior', 'Leisure'),
+	(3, 'Shooting Range', 'Exterior', 'Occupational'),
+	(4, 'Kitchen', 'Interior', 'Core'),
+	(5, 'Library', 'Interior', 'Leisure'),
+	(6, 'Office', 'Interior', 'Occupational'),
+	(7, 'Storage Room', 'Underground', 'Core'),
+	(8, 'Theater', 'Underground', 'Leisure'),
+	(9, 'Workshop', 'Underground', 'Occupational');
+
+INSERT INTO victims (id, name, tag1, tag2) VALUES
+	(1, 'Nora Perez', 'Retired', 'Lower-Class'),
+	(2, 'Emmett Parker', 'Retired', 'Middle-Class'),
+	(3, 'Arturo Elliot', 'Retired', 'Upper-Class'),
+	(4, 'Scott Walters', 'Student', 'Lower-Class'),
+	(5, 'Estelle Woods', 'Student', 'Middle-Class'),
+	(6, 'Warren Fisher', 'Student', 'Upper-Class'),
+	(7, 'Earl Daniels', 'Working', 'Lower-Class'),
+	(8, 'Stephanie McBride', 'Working', 'Middle-Class'),
+	(9, 'Gregg Washington', 'Working', 'Upper-Class');
