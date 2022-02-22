a142c4c69b3051f75207da99fb484cb004a085dc
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-02-21 20:16:14 -0500

    Added CORS override

--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, after_request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from api.db_utils import *
 from api.Culprit_api import *
 
 app = Flask(__name__) #create Flask instance
+CORS(app)
 
 api = Api(app) #api router
 
